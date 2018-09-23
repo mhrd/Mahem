343614686d7cecca55a9a8268630538ab191c485
mohsal110 <42447004+mohsal110@users.noreply.github.com>
2018-09-23 18:45:18 +0330

    Add files via upload

--- app/src/main/java/com/eagle/anonymous/mahem/MenuLineFragment.java
+++ app/src/main/java/com/eagle/anonymous/mahem/MenuLineFragment.java
@@ -9,10 +9,10 @@ import android.view.ViewGroup;
 
 
 
-public class MenuFragment extends Fragment {
+public class MenuLineFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_menu2,container,false);
+        return inflater.inflate(R.layout.activity_group,container,false);
     }
 }
