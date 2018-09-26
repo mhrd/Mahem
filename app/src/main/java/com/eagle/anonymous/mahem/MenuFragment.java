8ba107c02a53139c3eda15934a84f84b1be2576d
mohsal110 <42447004+mohsal110@users.noreply.github.com>
2018-09-26 07:24:20 +0330

    Update MenuFragment.java

--- app/src/main/java/com/eagle/anonymous/mahem/MenuFragment.java
+++ app/src/main/java/com/eagle/anonymous/mahem/MenuFragment.java
@@ -13,6 +13,9 @@ public class MenuFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_menu2,container,false);
+        View menuView= inflater.inflate(R.layout.activity_menu2,container,false);
+        
+        
+        return menuView;
     }
 }
