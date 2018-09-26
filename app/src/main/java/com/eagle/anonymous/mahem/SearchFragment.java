3641a535f8cfa2bfb1e73bcdc7ce1762ae83bf0d
mohsal110 <42447004+mohsal110@users.noreply.github.com>
2018-09-26 07:14:55 +0330

    Update SearchFragment.java

--- app/src/main/java/com/eagle/anonymous/mahem/SearchFragment.java
+++ app/src/main/java/com/eagle/anonymous/mahem/SearchFragment.java
@@ -9,9 +9,43 @@ import android.view.ViewGroup;
 
 
 public class SearchFragment extends Fragment {
+    
+    View navigationBar;
+    ImageView Home,Add,Menu,MenuLine,Search;
+    ImageView Filter;
+
+    RecyclerView recyclerView;
+    List<Advertising> AdvList;
+    AdvAdapter adapter;
+
+
+    //fill this variables with information
+    private String title, describtion, time;
+    private int image;
+    ///////////
+    
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_search,container,false);
+        View searchView= inflater.inflate(R.layout.activity_search,container,false);
+        AdvList=new ArrayList<>();
+        recyclerView=(RecyclerView)searchView.findViewById(R.id.RecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ///this line add search views to the list:
+        AdvList.add(new Advertising(title,describtion,time,image));
+        adapter=new AdvAdapter(this,AdvList);
+        recyclerView.setAdapter(adapter);
+
+        Filter=(ImageView)searchView.findViewById(R.id.filter);
+        Filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Search.this,Filter_other.class);
+                startActivity(i);
+
+            }
+        });
+        return searchView;
     }
 }
