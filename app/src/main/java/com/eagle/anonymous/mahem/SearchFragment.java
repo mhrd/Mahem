7c1e542af6ab48b827ca2b45e26692c1cf9edc3d
mohsal110 <42447004+mohsal110@users.noreply.github.com>
2018-09-29 19:56:38 +0330

    Update SearchFragment.java

--- app/src/main/java/com/eagle/anonymous/mahem/SearchFragment.java
+++ app/src/main/java/com/eagle/anonymous/mahem/SearchFragment.java
@@ -10,39 +10,74 @@ import android.view.ViewGroup;
 
 public class SearchFragment extends Fragment {
     
-    View navigationBar;
-    ImageView Home,Add,Menu,MenuLine,Search;
-    ImageView Filter;
+   ImageView Filter,search_btn;
 
     RecyclerView recyclerView;
-    List<Advertising> AdvList;
+    ArrayList<Advertising> AdvList;
     AdvAdapter adapter;
 
 
-    //fill this variables with information
-    private String title, describtion, time;
-    private int image;
-    ///////////
+    
+
+
+    private ArrayList<String> title=new ArrayList<String>() {
+
+    };
+    private ArrayList<String> describtion=new ArrayList<String>() {
+
+    };
+    private ArrayList<String> time=new ArrayList<String>() {
+
+    };
+
+
+    private ArrayList<Integer> image= new ArrayList<Integer>() {
+    };
+
+
+
+
+
+    ///////intialize counter variable that show how many (takhfif agahi) will be shown in layout
+    int counter=3;
+    ///
     
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View searchView= inflater.inflate(R.layout.activity_search,container,false);
-        AdvList=new ArrayList<>();
-        recyclerView=(RecyclerView)searchView.findViewById(R.id.RecyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        ///this line add search views to the list:
-        AdvList.add(new Advertising(title,describtion,time,image));
-        adapter=new AdvAdapter(this,AdvList);
-        recyclerView.setAdapter(adapter);
+        AdvList= new ArrayList<>();
+
+        for(int i=0;i<counter;++i) {
+            title.add("ii");
+            describtion.add("jj");
+            time.add("yy");
+            image.add(R.drawable.add);
+            recyclerView = (RecyclerView) searchView.findViewById(R.id.RecyclerView);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            ///this line add search views to the list:
+            AdvList.add(new Advertising(title.get(i), describtion.get(i), time.get(i), image.get(i)));
+            adapter = new AdvAdapter(getContext(), AdvList);
+            recyclerView.setAdapter(adapter);
+        }
 
         Filter=(ImageView)searchView.findViewById(R.id.filter);
         Filter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(Search.this,Filter_other.class);
-                startActivity(i);
+                //Intent i=new Intent(Search.this,Filter_other.class);
+                //startActivity(i);
+                Toast.makeText(getContext(),"clicked",Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
+        search_btn=(ImageView)searchView.findViewById(R.id.search_icon);
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
             }
         });
